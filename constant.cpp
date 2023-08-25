301d1130a3198585607822555b88e757eef67c27
Akib Mahmud <101253688+mahmud-akib@users.noreply.github.com>
2023-08-26 02:08:02 +0600

    Add files via upload

---
+++ constant.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=4;
+    const int b = 5; // Here b is constant value
+    cout<<"The value of b was : "<<b<<endl;
+   // b = 5;
+    cout<<"The value of b is : "<<b<<endl;
+    
+    return 0;
+}
