2ca1c42b879df015bbf5e1a4844c2556b2b45bf4
Akib Mahmud <101253688+mahmud-akib@users.noreply.github.com>
2023-08-26 01:55:08 +0600

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int fibo(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    else{
+        return fibo(n-1) + fibo(n-2);
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+    for(int i=0; i<n; i++){
+        int series = fibo(i);
+        cout<<series<<endl;
+    }
+    return 0;
+}
